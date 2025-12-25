3d70ae8a06ae7c6646c564e38bd2efb4d982ffcf
Ravagraid <levarielravagraiddemorial@gmail.com>
2025-12-25 03:17:00 +0000

    Removed junk

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -1,18 +1,17 @@
 return {
-	-- Command and arguments to start the server.
 	cmd = { "lua-language-server" },
-
-	-- Filetypes to automatically attach to.
 	filetypes = { "lua" },
-
-	-- Specific settings to send to the server. The schema for this is
-	-- defined by the server. For example the schema for lua-language-server
-	-- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
 	settings = {
 		Lua = {
+			completion = {
+				callSnippet = "Replace",
+			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
+			},
+			runtime = {
+				version = "LuaJIT",
 			},
 		},
 	},
