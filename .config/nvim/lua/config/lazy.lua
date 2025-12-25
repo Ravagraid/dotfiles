31b9324797183efd7a905fb0d826471772a94206
Ravagraid <levarielravagraiddemorial@gmail.com>
2025-12-25 00:59:34 +0000

    rewrote wordcount/reading time

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
-		{ import = "plugins.lang" },
 	},
 	checker = {
 		enabled = true,
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,17 +1,24 @@
+local function wordcount() return tostring(vim.fn.wordcount().words) .. "words" end
+local function readingtime()
+	return tostring(math.ceil(vim.fn.wordcount().words / 200.0)) .. " min"
+end
+local function is_markdown()
+	return vim.bo.filetype == "markdown" or vim.bo.filetype == "asciidoc"
+end
+
 return {
 	--Statusline
 	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
-			local prose = require("nvim-prose")
 			require("lualine").setup({
 				sections = {
 					lualine_a = { "mode" },
 					lualine_b = {
 						{ "filename", path = 1 },
 						{ "filesize" },
-						{ prose.word_count, cond = prose.is_available },
-						{ prose.reading_time, cond = prose.is_available },
+						{ wordcount, cond = is_markdown },
+						{ readingtime, cond = is_markdown },
 					},
 					lualine_c = { "diagnostics", "diff" },
 					lualine_x = { "encoding", "fileformat" },
@@ -22,28 +29,6 @@ return {
 		end,
 	},
 
-	--Terminal
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("toggleterm").setup({
-				open_mapping = [[<c-\>]],
-				direction = "float",
-				shell = "nu",
-				float_opts = {
-					border = "curved",
-				},
-			})
-
-			-- lazygit toggle
-			vim.api.nvim_set_keymap("n", "<c-g>", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-			function _lazygit_toggle()
-				require("toggleterm.terminal").Terminal
-					:new({ cmd = "lazygit", hidden = true, direction = "float" })
-					:toggle()
-			end
-		end,
-	},
 	{
 		"folke/noice.nvim",
 		dependencies = {
@@ -86,13 +71,19 @@ return {
 		},
 		priority = 10000,
 		lazy = false,
-		init = function() require("legendary").setup({ extensions = { lazy_nvim = true } }) end,
+		init = function()
+			require("legendary").setup({ extensions = { lazy_nvim = true } })
+		end,
 	},
 	{
 		"kristijanhusak/vim-dadbod-ui",
 		dependencies = {
 			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+			{
+				"kristijanhusak/vim-dadbod-completion",
+				ft = { "sql", "mysql", "plsql" },
+				lazy = true,
+			},
 		},
 		cmd = {
 			"DBUI",
