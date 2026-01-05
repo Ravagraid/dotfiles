e7721eed172beee1a7b81f279062c7de064ba903
Ravagraid <levarielravagraiddemorial@gmail.com>
2026-01-05 04:18:53 +0000

    refactor(diagnostics): removed bloat

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,51 +1,41 @@
+vim.lsp.enable('lua_ls')
+
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if
-			client
-			and client:supports_method(
-				vim.lsp.protocol.Methods.textDocument_completion
-			)
-		then
-			vim.opt.completeopt =
-				{ 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-			vim.lsp.completion.enable(
-				true,
-				client.id,
-				ev.buf,
-				{ autotrigger = true }
-			)
-			vim.keymap.set(
-				'i',
-				'<C-Space>',
-				function() vim.lsp.completion.get() end
-			)
-		end
-	end,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if
+        client
+        and client:supports_method(
+          vim.lsp.protocol.Methods.textDocument_completion
+        )
+    then
+      vim.opt.completeopt =
+      { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(
+        true,
+        client.id,
+        ev.buf,
+        { autotrigger = true }
+      )
+      vim.keymap.set(
+        'i',
+        '<C-Space>',
+        function() vim.lsp.completion.get() end
+      )
+    end
+  end,
 })
 
 -- Diagnostics
 vim.diagnostic.config({
-	underline = false,
-	virtual_text = false,
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = '',
-			[vim.diagnostic.severity.WARN] = '',
-			[vim.diagnostic.severity.INFO] = '',
-			[vim.diagnostic.severity.HINT] = '',
-		},
-		-- linehl = {
-		-- 	[vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-		-- 	[vim.diagnostic.severity.WARN] = 'WarningMsg',
-		-- 	[vim.diagnostic.severity.INFO] = 'InfoMsg',
-		-- 	[vim.diagnostic.severity.HINT] = 'HintMsg',
-		-- },
-		-- numhl = {
-		-- 	[vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-		-- 	[vim.diagnostic.severity.WARN] = 'WarningMsg',
-		-- 	[vim.diagnostic.severity.INFO] = 'InfoMsg',
-		-- 	[vim.diagnostic.severity.HINT] = 'HintMsg',
-		-- },
-	},
+  underline = false,
+  virtual_text = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+  },
 })
