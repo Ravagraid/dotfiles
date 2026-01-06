5906ca1f17ed5a6450e523cd6cd67040c2987df7
Ravagraid <levarielravagraiddemorial@gmail.com>
2026-01-06 17:11:08 +0000

    refactor: lspsaga

--- .config/nvim/lsp/markdown_oxide.lua
+++ .config/nvim/lsp/markdown_oxide.lua
@@ -1,4 +1,5 @@
 return {
 	cmd = { 'markdown-oxide' },
 	filetypes = { 'markdown' },
+	root_markers = { '.git', '.obsidian', '.moxide.toml' },
 }
---
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,58 @@
+return {
+	'nvimdev/lspsaga.nvim',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter',
+		'nvim-tree/nvim-web-devicons',
+	},
+	keys = {
+		--code
+		{ '<leader>ca', '<cmd>Lspsaga code_action<cr>', desc = 'code action' },
+		{
+			'<leader>ch',
+			'<cmd>Lspsaga hover_doc<cr>',
+			desc = 'hover diagnostics',
+		},
+		{ '<leader>co', '<cmd>Lspsaga outline<cr>', desc = 'code outline' },
+		{ '<leader>cr', '<cmd>Lspsaga rename<cr>', 'code rename' },
+
+		--next/prev
+		{
+			']d',
+			'<cmd>Lspsaga diagnostic_jump_next<cr>',
+			desc = 'next diagnostic',
+		},
+		{
+			'[d',
+			'<cmd>Lspsaga diagnostic_jump_prev<cr>',
+			desc = 'previous diagnostic',
+		},
+
+		--goto
+		{
+			'gd',
+			'<cmd>Lspsaga goto_definition<cr>',
+			desc = 'goto defintion',
+		},
+	},
+	config = function()
+		require('lspsaga').setup({
+			diagnostic = {
+				border_follow = false,
+			},
+			symbol_in_winbar = {
+				show_file = false,
+				delay = 0,
+			},
+			ui = {
+				border = 'rounded',
+				code_action = '',
+			},
+		})
+		vim.keymap.set(
+			'n',
+			'<space>k',
+			'<cmd>Lspsaga hover_doc<cr>',
+			{ silent = true }
+		)
+	end,
+}
