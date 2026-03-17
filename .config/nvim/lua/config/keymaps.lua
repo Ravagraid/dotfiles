f75857c795237a284fc9a123d4bed9a74889b3ce
Ravagraid <levarielravagraiddemorial@gmail.com>
2026-03-17 11:57:44 +0000

    Changed Keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,26 +4,57 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- better up/down
-key.set('n', 'j', 'gj')
-key.set('n', 'k', 'gk')
+key.set(
+	'n',
+	'j',
+	function() return vim.v.count == 0 and 'gj' or 'j' end,
+	{ expr = true, silent = true, desc = 'Down (wrap-aware)' }
+)
 
--- windows
-key.set('n', '<leader>ww', '<C-W>p', { desc = 'other window' })
-key.set('n', '<leader>wd', '<C-W>c', { desc = 'delete window' })
-key.set('n', '<leader>w-', '<C-W>s', { desc = 'split window below' })
-key.set('n', '<leader>w|', '<C-W>v', { desc = 'split window right' })
+key.set(
+	'n',
+	'k',
+	function() return vim.v.count == 0 and 'gk' or 'k' end,
+	{ expr = true, silent = true, desc = 'Up (wrap-aware)' }
+)
 
--- cursor
-key.set({ 'i', 'v', 'n', 's' }, '<C-s>', ':w<CR><esc>', { desc = 'Save' })
-
---Misc QOL maps
-key.set('n', '<c-a>', 'ggVG', { desc = 'Select all' })
 key.set('n', '<Leader><space>', ':nohl<CR>', { desc = 'Clear search' })
+key.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
+key.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
+
+key.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page down' })
+key.set('n', '<C-u>', '<C-u>zz', { desc = 'Half page up' })
+
+key.set('x', '<leader>p', '"_dP"', { desc = 'Paste without yanking' })
+key.set({ 'n', 'v' }, '<leader>d', '"_d', { desc = 'Delete without yanking' })
+
+key.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+key.set('n', '<C-j>', '<C-w>j', { desc = 'Move to bottom window' })
+key.set('n', '<C-k>', '<C-w>k', { desc = 'Move to top window' })
+key.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
+key.set('n', '<leader>sv', ':vsplit<CR>', { desc = 'Split window vertically' })
+key.set('n', '<leader>sh', ':split<CR>', { desc = 'Split window horizontally' })
+key.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
+key.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
+key.set(
+	'n',
+	'<C-Left>',
+	':vertical resize -2<CR>',
+	{ desc = 'Decrease window width' }
+)
+key.set(
+	'n',
+	'<C-Right>',
+	':vertical resize +2<CR>',
+	{ desc = 'Increase window width' }
+)
+
+key.set('n', '<c-a>', 'ggVG', { desc = 'Select all' })
 key.set({ 'i', 'v', 'n', 's' }, '<C-s>', ':w<CR><esc>', { desc = 'Save' })
 key.set('n', '<leader>qq', ':qa<CR>', { desc = 'quit' })
 key.set('n', '<leader>qw', ':q!<CR>', { desc = 'quit w/o save' })
 
---Lazy and plugin maps
 key.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Lazy UI' })
 key.set(
 	'n',
