af2da960a6248c7b15138b18d8097f54ebf4b3de
Ravagraid <levarielravagraiddemorial@gmail.com>
2026-04-01 07:17:26 +0000

    [mason] added tool-installer

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,12 +1,36 @@
 return {
-	'mason-org/mason.nvim',
-	opts = {
-		ui = {
-			icons = {
-				package_installed = '✓',
-				package_pending = '➜',
-				package_uninstalled = '✗',
+	'WhoIsSethDaniel/mason-tool-installer.nvim',
+	dependencies = {
+		'mason-org/mason.nvim',
+		opts = {
+			ui = {
+				icons = {
+					package_installed = '',
+					package_pending = '➜',
+					package_uninstalled = '✗',
+				},
 			},
 		},
+	},
+	opts = {
+		ensure_installed = {
+			'bash-language-server',
+			'beautysh',
+			'clang-format',
+			'clangd',
+			'json-lsp',
+			'kdlfmt',
+			'lua-language-server',
+			'markdown-oxide',
+			'mdformat',
+			'prettier',
+			'stylua',
+			'systemd-lsp',
+			'systemdlint',
+			'tinymist',
+			'typstyle',
+		},
+		auto_update = true,
+		start_delay = 3000,
 	},
 }
