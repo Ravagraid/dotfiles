2bca244bfe7ecdafaef0d3b9c0cdea3de0e80c2a
Ravagraid <levarielravagraiddemorial@gmail.com>
2026-08-22 19:03:17 +0100

    [nvim] removed mini.move

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,17 +5,6 @@ return {
 		local miniclue = require('mini.clue')
 		-- Text editing
 		require('mini.comment').setup()
-		require('mini.move').setup({
-			left = 'H',
-			right = 'L',
-			down = 'J',
-			up = 'K',
-
-			line_left = 'H',
-			line_right = 'L',
-			line_down = 'J',
-			line_up = 'K',
-		})
 		require('mini.pairs').setup({
 			mappings = {
 				['*'] = {
